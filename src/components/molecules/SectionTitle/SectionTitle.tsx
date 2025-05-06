cd3dc30d133aff9524c88b5358c6d5a99c60d242
quocma-tbs <quocma@techbee.dev>
2025-05-06 16:31:49 +0700

    feat: init home page

---
+++ src/components/molecules/SectionTitle/SectionTitle.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { SectionHeading } from '@/components/atoms/Heading';
+import { SectionSubHeading } from '@/components/atoms/SubHeading';
+
+type SectionTitleProps = {
+  heading: string;
+  subHeading: string;
+};
+
+const SectionTitle = ({ heading, subHeading }: SectionTitleProps) => {
+  return (
+    <div>
+      <SectionHeading className="text-center">{heading}</SectionHeading>
+      <SectionSubHeading>{subHeading}</SectionSubHeading>
+    </div>
+  );
+};
+
+export default SectionTitle;
---
+++ src/components/organisms/Home/Intro/Intro.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import SectionTitle from '@/components/molecules/SectionTitle/SectionTitle';
+
+const Intro = () => {
+  return (
+    <section className="py-10">
+      <SectionTitle
+        heading="Về Thương Hiệu Chúng Tôi"
+        subHeading="Cam kết chất lượng, vệ sinh an toàn thực phẩm và giá trị dinh dưỡng cao"
+      />
+    </section>
+  );
+};
+
+export default Intro;
