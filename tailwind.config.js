fe6d61ab1dcbfe06a0f4287a97f6e7574752e99a
quocma-tbs <quocma@techbee.dev>
2025-05-06 16:27:55 +0700

    feat: setup base style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,39 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: 'jit',
   content: ['./src/app/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      height: {
+        header: 'var(--header-height)',
+        content: 'calc(100vh - var(--header-height))'
+      },
+      spacing: {
+        'header-height': 'var(--header-height)'
+      },
+      zIndex: {
+        top: 'var(--top-layer-z-index)'
+      },
       fontFamily: {
         primary: ['var(--font-primary)']
+      },
+      colors: {
+        primary: {
+          DEFAULT: '#D62C35'
+        },
+        secondary: {
+          DEFAULT: '#FFEB70'
+        },
+        typo: {
+          1: '#424B5A'
+        }
+      },
+      backgroundImage: {
+        'primary-gradient-90': 'linear-gradient(90deg, #920202 0%, #B4071A 100%)',
+        'primary-dark-gradient-360': 'linear-gradient(360deg, #670000 0%, #920202 100%)',
+        'secondary-gradient-180': 'linear-gradient(180deg, #E6B522 0%, #FFF788 47% ,#FFE059 100%)',
+        'secondary-gradient-90': 'linear-gradient(90deg, #E6B522 0%, #FFF788 47% ,#FFE059 100%)'
       }
     }
   },
