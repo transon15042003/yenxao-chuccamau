ca5cf19d7acc2705c0f2df95f96b397a3f069921
quocma-tbs <quocma@techbee.dev>
2025-05-06 16:31:29 +0700

    feat: change default font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import '@/styles/globals.css';
 
 import { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Nunito_Sans } from 'next/font/google';
 import { ReactNode } from 'react';
 
 import { MainProvider } from '@/components/providers/MainProvider';
@@ -9,12 +9,11 @@ import { MainLayout } from '@/components/templates/MainLayout';
 
 import { cn } from '@/lib/utils';
 
-const inter = Inter({ subsets: ['latin'], variable: '--font-primary' });
+const nunitoSans = Nunito_Sans({ subsets: ['latin'], variable: '--font-primary' });
 
 export const metadata: Metadata = {
-  title: 'Home page | Nextjs boilerplate',
-  description:
-    '🚀 Boilerplate and Starter for Next.js, Tailwind CSS and TypeScript ⚡️ Made with developer experience first: Next.js, TypeScript, ESLint, Prettier, Husky, Lint-Staged, Jest, React Testing Library, PostCSS, Tailwind CSS, Storybook, Plop, GH actions.'
+  title: 'Trang chủ | Chuc Ca Mau',
+  description: 'seo description'
 };
 
 interface RootLayoutProps {
@@ -24,7 +23,7 @@ interface RootLayoutProps {
 const RootLayout = ({ children }: RootLayoutProps) => {
   return (
     <html lang="en">
-      <body className={cn(inter.variable, 'font-primary')} suppressHydrationWarning>
+      <body className={cn(nunitoSans.variable, 'font-primary')} suppressHydrationWarning>
         <MainProvider>
           <MainLayout>
             <main>{children}</main>
