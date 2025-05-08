18717dd1fb39e19fbda27d4a0801e5f025083031
quocma-tbs <quocma@techbee.dev>
2025-05-08 11:14:22 +0700

    feat: about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Button } from '@/components/atoms/Button/Button';
+
+const AboutPage = () => {
+  return (
+    <div>
+      <Button className="bg-red-500">Example</Button>
+    </div>
+  );
+};
+
+export default AboutPage;
