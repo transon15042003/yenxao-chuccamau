55c493131d68608fa949e8a36dadb929571b0e9e
KhuongVoTechbee <khuongvo@techbee.dev>
2025-05-11 22:52:15 +0700

    style: update header

--- src/components/molecules/NavigationMenu/NavigationMenu.tsx
+++ src/components/molecules/NavigationMenu/NavigationMenu.tsx
@@ -1,27 +1,18 @@
 import Link from 'next/link';
 import React from 'react';
 
-import { SearchBar } from '@/components/atoms/SearchBar';
+type NavigationProps = {
+  className?: string;
+};
 
-const NavigationMenu = () => {
+const NavigationMenu = (props: NavigationProps) => {
   return (
-    <div className="flex items-center gap-4">
-      <Link className="hidden md:inline" href="/">
-        Home
-      </Link>
-      <Link className="hidden md:inline" href="/about">
-        About
-      </Link>
-      <Link className="hidden md:inline" href="/contact">
-        Contact
-      </Link>
-      <Link className="hidden md:inline" href="/#">
-        Item (atom)
-      </Link>
-      <Link className="hidden md:inline" href="/#">
-        Item with dropdown (molecule)
-      </Link>
-      <SearchBar className="" placeholder="Tim kiem" />
+    <div className={`${props.className} flex items-center gap-4`}>
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/contact">Contact</Link>
+      <Link href="/#">Item (atom)</Link>
+      <Link href="/#">Item with dropdown (molecule)</Link>
     </div>
   );
 };
--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -3,16 +3,18 @@ import React from 'react';
 import { Cart } from '@/components/atoms/Cart';
 import Logo from '@/components/atoms/Logo/Logo';
 import { Menu } from '@/components/atoms/Menu';
+import { SearchBar } from '@/components/atoms/SearchBar';
 import NavigationMenu from '@/components/molecules/NavigationMenu/NavigationMenu';
 const Header = () => {
   return (
-    <header className="w-full h-header bg-primary-gradient-90 text-white fixed top-0 z-top">
+    <header className="flex w-full h-header bg-primary-gradient-90 text-white fixed top-0 z-top">
       <div className="flex h-full items-center justify-between w-full mx-10 xl:mx-auto xl:w-[93%]">
         <Logo className="w-1/3" />
-        <NavigationMenu />
-        <div className="w-1/3 flex-col justify-evenly items-center">
-          <Cart className="w-1/2" count={3} />
-          <Menu className="w-1/2 md:hidden" />
+        <NavigationMenu className="hidden md:inline w-1/3" />
+        <div className="w-2/3 md:w-1/6 flex justify-end items-center">
+          <SearchBar className="grow" placeholder="Tim kiem" />
+          <Cart className="flex-none" count={3} />
+          <Menu className="flex-none md:hidden" />
         </div>
       </div>
     </header>
