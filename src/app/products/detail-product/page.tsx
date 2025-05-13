0a5f67e013f8c0666740792645d9dc964d0edf63
BangVo123 <vochebang.123@gmail.com>
2025-05-13 08:58:26 +0700

    feat: implement product detail

--- src/app/products/detail-product/page.tsx
+++ src/app/products/detail-product/page.tsx
@@ -1,22 +1,5 @@
 import DetailProduct from '@/components/templates/DetailProduct/DetailProduct';
 
-const DetailProductPage = () => (
-  // <div className="grid grid-cols-12 gap-2">
-  //   <div className="col-span-1 inline-block">
-  //     <Image className="w-full h-auto" width={100} height={100} src="/product.png" alt="product" />
-  //     <Image className="w-full h-auto" width={100} height={100} src="/product.png" alt="product" />
-  //     <Image className="w-full h-auto" width={100} height={100} src="/product.png" alt="product" />
-  //     <Image className="w-full h-auto" width={100} height={100} src="/product.png" alt="product" />
-  //     <Image className="w-full h-auto" width={100} height={100} src="/product.png" alt="product" />
-  //   </div>
-  //   <div className="col-span-5 inline-block">
-  //     <Image className="w-full h-auto" width={100} height={100} src="/product.png" alt="product" />
-  //   </div>
-  //   <div className="col-span-6 inline-block">Action</div>
-  // </div>
-  <>
-    <DetailProduct />
-  </>
-);
+const DetailProductPage = () => <DetailProduct />;
 
 export default DetailProductPage;
