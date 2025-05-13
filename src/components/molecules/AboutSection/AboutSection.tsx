74b554e46fc57e6e5d3eae2002e710d51bb652a2
KhuongVoTechbee <khuongvo@techbee.dev>
2025-05-14 00:56:25 +0700

    chore: merge Intro & Certificates to AboutSection component

---
+++ src/components/molecules/AboutSection/AboutSection.tsx
@@ -0,0 +1,23 @@
+import Intro from '@/components/organisms/Home/Intro/Intro';
+
+import { Certificates } from '../Certificates';
+
+export const AboutSection = () => (
+  <div className="w-full h-auto md:h-[917px] flex flex-col items-center justify-around py-[70px] relative overflow-hidden">
+    {/* Phần tử div riêng cho ảnh nền, xoay và làm mờ */}
+    <div
+      className="absolute w-full h-full
+                   bg-[url('/scrath.png')] bg-contain bg-bottom bg-no-repeat opacity-[.07] z-0"
+      style={{
+        transform: 'rotate(65.33deg) scale(1.5)',
+        top: '-120px',
+        left: '-600px'
+      }}
+    >
+      {/* Div này chỉ dùng cho hiệu ứng nền */}
+    </div>
+
+    <Intro />
+    <Certificates />
+  </div>
+);
