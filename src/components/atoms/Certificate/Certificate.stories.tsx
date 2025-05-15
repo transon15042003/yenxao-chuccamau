6595a47c2f56cdf5ef87e6dff342b30e377fb4dc
KhuongVoTechbee <khuongvo@techbee.dev>
2025-05-15 12:04:12 +0700

    refactor: update some issues

--- src/components/atoms/Certificate/Certificate.stories.tsx
+++ src/components/atoms/Certificate/Certificate.stories.tsx
@@ -1,3 +1,4 @@
+import { NaturalSVG } from '@/svg/CertificateSVG/NaturalSVG';
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { Certificate } from './Certificate';
@@ -7,15 +8,10 @@ const meta: Meta<typeof Certificate> = {
   component: Certificate,
   tags: ['autodocs'],
   args: {
-    certificateSrc: '/certificates/Group 8245.svg',
-    backgroundSrc: '',
     title: 'Vệ Sinh',
     subTitle: 'Sản xuất đạt tiêu chuẩn',
-    boxWidth: 384,
-    boxHeight: 386,
-    certificateWidth: 130,
-    certificateHeight: 122,
-    className: ''
+    className: '',
+    SvgIconComponent: <NaturalSVG />
   }
 };
 
