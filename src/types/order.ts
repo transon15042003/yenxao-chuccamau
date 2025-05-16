4a73b6af1dd550447e6f448469bac4ecbebec4a1
quocma-tbs <quocma@techbee.dev>
2025-05-16 13:47:52 +0700

    feat: define order & payment types

---
+++ src/types/order.ts
@@ -0,0 +1,33 @@
+import { CartItem } from '@/types/cart';
+
+export type ShippingMethod = 'STANDARD' | 'WORKING_HOURS';
+export type Order = {
+  _id?: string;
+  code: string;
+  status: OrderStatus;
+  items: CartItem[];
+  customer: OrderCustomer;
+  invoice?: OrderInvoice;
+  note?: string;
+  paymentStatus: OrderPaymentStatus;
+  paidAt?: Date;
+  shippingMethod: ShippingMethod;
+};
+
+export type OrderCustomer = {
+  name: string;
+  email?: string;
+  phone: string;
+  province?: string;
+  district?: string;
+  address: string;
+};
+
+export type OrderStatus = 'pending' | 'processing' | 'completed' | 'cancelled';
+export type OrderPaymentStatus = 'pending' | 'paid';
+export type OrderInvoice = {
+  name: string;
+  email: string;
+  taxCode: string;
+  address: string;
+};
