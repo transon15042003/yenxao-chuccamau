2a721197f27686ec0afc9d32d50058299435f7d5
BangVo123 <vochebang.123@gmail.com>
2025-05-21 16:20:55 +0700

    fix: img comp

--- src/components/organisms/ProductInfoPanel/ProductInfoPanel.tsx
+++ src/components/organisms/ProductInfoPanel/ProductInfoPanel.tsx
@@ -1,6 +1,7 @@
 // import Discount from '@/components/atoms/Discount/Discount';
+import Image from 'next/image';
+
 import { Badge } from '@/components/atoms/Badge';
-import Img from '@/components/atoms/Image/Image';
 import SliderControl from '@/components/molecules/SliderControl/SliderControl';
 
 import { cn } from '@/lib/utils';
@@ -11,7 +12,13 @@ interface ProductInfoPanelProps {
 
 const ProductInfoPanel = ({ className }: ProductInfoPanelProps) => (
   <div className={cn('relative w-full h-auto', className)}>
-    <Img src="/product.png" alt="product" className="h-full object-cover" />
+    <Image
+      width={100}
+      height={100}
+      src="/product.png"
+      alt="product"
+      className="h-full w-full object-cover"
+    />
     {/* <Discount discount={20} className="absolute top-[10px] left-[10px] w-[55px] text-center" /> */}
     <Badge content="20%" className="absolute top-2 left-2" />
     <SliderControl className="absolute bottom-[10px] right-[10px] text-[#424B5A]" />
