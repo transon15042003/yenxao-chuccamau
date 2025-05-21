326b336018e2371f94f610c878f9ce0216ba65b3
Trần Hoàng Sơn <sontran@techbee.dev>
2025-05-22 01:45:29 +0700

    refactor: create a template for policy pages

--- src/components/templates/ReturnPolicy/ReturnPolicy.tsx
+++
@@ -1,9 +0,0 @@
-import ParagraphPolicy from '@/components/molecules/ParagraphPolicy/ParagraphPolicy';
-
-const ReturnPolicy = () => (
-  <div className="bg-[url('/images/policy/bg.png')] bg-no-repeat bg-cover h-full w-full px-2 py-[48px] lg:grid lg:grid-cols-12">
-    <ParagraphPolicy />
-  </div>
-);
-
-export default ReturnPolicy;
