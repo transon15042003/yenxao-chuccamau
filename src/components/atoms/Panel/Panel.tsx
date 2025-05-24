09ff26e9bf2b7d593cec5580e0599df7e897b1f0
KhuongVoTechbee <khuongvo@techbee.dev>
2025-05-24 16:20:12 +0700

    style: :iphone: re-fix hero panel responsive for each device screen

--- src/components/atoms/Panel/Panel.tsx
+++ src/components/atoms/Panel/Panel.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 
+import { cn } from '@/lib/utils';
+
 type PanelProps = {
   imageSrc?: string;
   className?: string;
@@ -9,20 +11,21 @@ export const Panel = (props: PanelProps) => {
   return (
     <div
       data-testid="panel-container"
-      className={`w-full overflow-hidden ${props.className || ''}`}
+      className={cn(
+        'relative w-full overflow-hidden',
+        props.className || '',
+        'h-[268px] lg:h-[583px]'
+      )}
     >
       <Image
         src={props.imageSrc || '/images/backgrounds/img_panel.svg'}
         alt="Panel background image"
-        className="scale-125 -translate-x-[40px] lg:translate-x-0 lg:scale-100 w-full h-auto"
-        width={1440}
-        height={583}
+        className={cn(
+          'absolute inset-0 w-full h-full object-cover',
+          'object-[90%_50%] lg:object-center'
+        )}
+        fill
         unoptimized={true}
-        style={
-          {
-            // transform:'scale(1.5)'
-          }
-        }
       />
     </div>
   );
