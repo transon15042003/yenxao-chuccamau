695843b64b72a152a75a79d26ef18d6380ceb6c1
BangVo123 <vochebang.123@gmail.com>
2025-05-27 10:25:31 +0700

    feat: add SEO content for contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,16 @@
+import { StaticSEOContent } from '@/contents/SEO';
+import { Metadata } from 'next';
 import { ContactPage as Page } from 'src/components/templates/ContactPage';
 import { getContactInfo } from 'src/services/contact.service';
+
+export const metadata: Metadata = {
+  title: StaticSEOContent.contactPage.title,
+  description: StaticSEOContent.contactPage.desc,
+  keywords: StaticSEOContent.contactPage.keywords,
+  alternates: {
+    canonical: StaticSEOContent.contactPage.canonicalUrl
+  }
+};
 
 const ContactPage = async () => {
   const contactData = await getContactInfo();
