1b397592b9e78eb08496a0cd01d9aa7281af6ef2
BangVo123 <vochebang.123@gmail.com>
2025-05-27 10:35:01 +0700

    feat: add SEO content for privacy policy page

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,6 +1,17 @@
 import { privacyPolicyContent } from '@/contents/privacyPolicy';
+import { StaticSEOContent } from '@/contents/SEO';
+import { Metadata } from 'next';
 
 import Policy from '@/components/templates/Policy/Policy';
+
+export const metadata: Metadata = {
+  title: StaticSEOContent.privacyPolicyPage.title,
+  description: StaticSEOContent.privacyPolicyPage.desc,
+  keywords: StaticSEOContent.privacyPolicyPage.keywords,
+  alternates: {
+    canonical: StaticSEOContent.privacyPolicyPage.canonicalUrl
+  }
+};
 
 const PrivacyPolicyPage = () => {
   return <Policy title="Chính Sách Bảo Mật" content={privacyPolicyContent} />;
