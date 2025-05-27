2647140bdb6c9c55f4cdda73f9d8850746236cc1
BangVo123 <vochebang.123@gmail.com>
2025-05-27 10:37:41 +0700

    feat: add SEO content for return policy page

--- src/app/return-policy/page.tsx
+++ src/app/return-policy/page.tsx
@@ -1,6 +1,17 @@
 import { returnPolicyContent } from '@/contents/returnPolicy';
+import { StaticSEOContent } from '@/contents/SEO';
+import { Metadata } from 'next';
 
 import Policy from '@/components/templates/Policy/Policy';
+
+export const metadata: Metadata = {
+  title: StaticSEOContent.returnPolicyPage.title,
+  description: StaticSEOContent.returnPolicyPage.desc,
+  keywords: StaticSEOContent.returnPolicyPage.keywords,
+  alternates: {
+    canonical: StaticSEOContent.returnPolicyPage.canonicalUrl
+  }
+};
 
 const ReturnPolicyPage = () => {
   return <Policy title="Chính Sách Kiểm Tra & Đổi Trả Hàng Hóa" content={returnPolicyContent} />;
