9c28fc706237c9f0cf899f2df4bf37536e4dd197
BangVo123 <vochebang.123@gmail.com>
2025-05-27 10:41:10 +0700

    feat: add SEO content for delivery policy page

--- src/app/shipping-policy/page.tsx
+++ src/app/shipping-policy/page.tsx
@@ -1,6 +1,17 @@
+import { StaticSEOContent } from '@/contents/SEO';
 import { shippingPolicyContent } from '@/contents/shippingPolicy';
+import { Metadata } from 'next';
 
 import Policy from '@/components/templates/Policy/Policy';
+
+export const metadata: Metadata = {
+  title: StaticSEOContent.deliveryPolicyPage.title,
+  description: StaticSEOContent.deliveryPolicyPage.desc,
+  keywords: StaticSEOContent.deliveryPolicyPage.keywords,
+  alternates: {
+    canonical: StaticSEOContent.deliveryPolicyPage.canonicalUrl
+  }
+};
 
 const ShippingPolicyPage = () => {
   return <Policy title="Chính Sách Vận Chuyển" content={shippingPolicyContent} />;
