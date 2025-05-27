5e28e42d8f19d212f0c511b68c62607423528969
BangVo123 <vochebang.123@gmail.com>
2025-05-27 11:51:41 +0700

    feat: add SEO content for each prod

--- src/app/products/[slug]/layout.tsx
+++ src/app/products/[slug]/layout.tsx
@@ -1,10 +1,23 @@
+import { dynamicProductContent } from '@/contents/SEO';
 import { Metadata } from 'next';
 import React from 'react';
 
-export const metadata: Metadata = {
-  title: 'Chi tiết sản phẩm',
-  description: 'Chi tiết sản phẩm'
-};
+export async function generateMetadata({
+  params
+}: {
+  params: { slug: string };
+}): Promise<Metadata> {
+  const seoContent = await dynamicProductContent(params.slug);
+
+  return {
+    title: seoContent.title,
+    description: seoContent.desc,
+    keywords: seoContent.keywords,
+    alternates: {
+      canonical: seoContent.canonicalUrl
+    }
+  };
+}
 
 const ProductDetailLayout = ({ children }: { children: React.ReactNode }) => {
   return <div className="bg-white">{children}</div>;
