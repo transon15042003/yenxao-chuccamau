2adf5fe43b98f504caa79edd158963d0d8f61add
BangVo123 <vochebang.123@gmail.com>
2025-05-27 10:32:09 +0700

    feat: add SEO content for payment policy page

--- src/app/payment-policy/page.tsx
+++ src/app/payment-policy/page.tsx
@@ -1,6 +1,17 @@
 import { paymentPolicyContent } from '@/contents/paymentPolicy';
+import { StaticSEOContent } from '@/contents/SEO';
+import { Metadata } from 'next';
 
 import Policy from '@/components/templates/Policy/Policy';
+
+export const metadata: Metadata = {
+  title: StaticSEOContent.paymentPolicyPage.title,
+  description: StaticSEOContent.paymentPolicyPage.desc,
+  keywords: StaticSEOContent.paymentPolicyPage.keywords,
+  alternates: {
+    canonical: StaticSEOContent.paymentPolicyPage.canonicalUrl
+  }
+};
 
 const PaymentPolicyPage = () => {
   return <Policy title="Chính Sách Thanh Toán" content={paymentPolicyContent} />;
