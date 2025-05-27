000a4c0668aeb1c9be3660a789f58f9b1f246899
BangVo123 <vochebang.123@gmail.com>
2025-05-27 10:29:45 +0700

    feat: add SEO content for order page

--- src/app/order/layout.tsx
+++ src/app/order/layout.tsx
@@ -1,11 +1,16 @@
+import { StaticSEOContent } from '@/contents/SEO';
 import { Metadata } from 'next';
 import React, { PropsWithChildren } from 'react';
 
 import PaymentPageProvider from './_context/PaymentPageContext';
 
 export const metadata: Metadata = {
-  title: 'Đặt hàng',
-  description: 'Đặt hàng'
+  title: StaticSEOContent.orderPage.title,
+  description: StaticSEOContent.orderPage.desc,
+  keywords: StaticSEOContent.orderPage.keywords,
+  alternates: {
+    canonical: StaticSEOContent.orderPage.canonicalUrl
+  }
 };
 
 const PaymentLayout = ({ children }: PropsWithChildren) => {
