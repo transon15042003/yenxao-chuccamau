c8d0a502cea989d2d14ae97a0e5f01bffa1070be
BangVo123 <vochebang.123@gmail.com>
2025-05-30 11:01:19 +0700

    fix: hardcode in page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -21,7 +21,7 @@ export async function generateMetadata({
     description: StaticSEOContent.productsPage.desc,
     keywords: StaticSEOContent.productsPage.keywords,
     alternates: {
-      canonical: StaticSEOContent.productsPage.canonicalUrl
+      canonical: `${process.env.NEXT_PUBLIC_APP_DOMAIN}/products`
     }
   };
 
