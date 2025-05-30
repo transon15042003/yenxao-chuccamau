ec75f95246d058ebe8fb619ce007755bf1baa184
quocma-tbs <quocma@techbee.dev>
2025-05-30 14:31:36 +0700

    feat: enhance product slider behavior

--- src/components/organisms/OthersProduct/OthersProduct.tsx
+++ src/components/organisms/OthersProduct/OthersProduct.tsx
@@ -5,32 +5,36 @@ import { LargeChevronLeftSVG } from '@/svg/LargeChevronLeftSVG/LargeChevronLeftS
 import { LargeChevronRightSVG } from '@/svg/LargeChevronRightSVG/LargeChevronRightSVG';
 import { Product } from '@/types/product';
 import { useRouter } from 'next/navigation';
-import { useRef } from 'react';
-import { Swiper as SwiperType } from 'swiper';
+import { useMemo, useRef } from 'react';
+import type { Swiper as SwiperType } from 'swiper';
+// eslint-disable-next-line import-helpers/order-imports
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-import 'swiper/css/pagination';
 import 'swiper/css';
+import 'swiper/css/pagination';
+
+import { useWindowSize } from 'usehooks-ts';
 
 import { Button } from '@/components/atoms/Button';
 import { SectionHeading } from '@/components/atoms/Heading';
 import { ProductCard } from '@/components/molecules/ProductCard';
 import { useCart } from '@/components/providers/CartProvider/CartProvider';
-import { useDetailProduct } from '@/components/providers/DetailProductProvider/DetailProductProvider';
 
+import { cn } from '@/lib/utils/cn';
 import { convertProductToCartItem } from '@/lib/utils/product';
 
 interface OthersProductProps {
   className?: string;
   heading: string;
+  products: Product[];
 }
 
-const OthersProduct = ({ className, heading }: OthersProductProps) => {
-  const { products } = useDetailProduct();
+const OthersProduct = ({ className, heading, products }: OthersProductProps) => {
   const { addToCart } = useCart();
   const { handleBuyNow } = useBuyNowLogic();
   const router = useRouter();
   const swiperRef = useRef<SwiperType | null>(null);
+  const { width } = useWindowSize({ initializeWithValue: false });
 
   const handleAddToCart = (product: Product) => {
     addToCart(convertProductToCartItem(product));
@@ -52,21 +56,40 @@ const OthersProduct = ({ className, heading }: OthersProductProps) => {
     if (swiperRef.current) swiperRef.current.slideNext();
   };
 
+  const isSwipeAvailable = useMemo(() => {
+    if (width && width > 1024) {
+      return products.length > 4;
+    }
+
+    return products.length > 2;
+  }, [width, products]);
+
   return (
     <div className={className}>
       <SectionHeading className="text-[#2A2A40] mb-2">{heading}</SectionHeading>
       <div className="relative">
         <div className="flex flex-row overflow-x-auto lg:overflow-x-hidden gap-4 lg:gap-8 max-h-[540px]">
-          <Button
-            variant="primary"
-            fill="fill"
-            onClick={handlePrev}
-            className="swiper-button-prev w-[40px] h-[40px] hidden lg:flex items-center justify-center rounded-full absolute [top:30%] left-0 -translate-x-1/2 z-10"
-          >
-            <LargeChevronLeftSVG className="text-white h-[24px] w-[24px] stroke-[5px]" />
-          </Button>
+          {isSwipeAvailable && (
+            <Button
+              variant="primary"
+              fill="fill"
+              onClick={handlePrev}
+              className={cn(
+                'swiper-button-prev',
+                'w-[40px] h-[40px]',
+                'hidden lg:flex',
+                'items-center justify-center',
+                'rounded-full',
+                'absolute [top:30%] left-0 -translate-x-1/2',
+                'z-10'
+              )}
+            >
+              <LargeChevronLeftSVG className="text-white h-[24px] w-[24px] stroke-[5px]" />
+            </Button>
+          )}
           <Swiper
-            loop={true}
+            navigation={isSwipeAvailable}
+            // loop={true}
             direction="horizontal"
             breakpoints={{
               0: {
@@ -103,14 +126,24 @@ const OthersProduct = ({ className, heading }: OthersProductProps) => {
               </SwiperSlide>
             ))}
           </Swiper>
-          <Button
-            variant="primary"
-            fill="fill"
-            onClick={handleNext}
-            className="swiper-button-next w-[40px] h-[40px] hidden lg:block lg:flex items-center justify-center rounded-full absolute [top:30%] right-0 translate-x-1/2 z-20"
-          >
-            <LargeChevronRightSVG className="text-white h-[24px] w-[24px] stroke-[5px]" />
-          </Button>
+          {isSwipeAvailable && (
+            <Button
+              variant="primary"
+              fill="fill"
+              onClick={handleNext}
+              className={cn(
+                'swiper-button-next',
+                'w-[40px] h-[40px]',
+                'hidden lg:flex',
+                'items-center justify-center',
+                'rounded-full',
+                'absolute [top:30%] right-0 translate-x-1/2 z-20',
+                !isSwipeAvailable && 'hidden'
+              )}
+            >
+              <LargeChevronRightSVG className="text-white h-[24px] w-[24px] stroke-[5px]" />
+            </Button>
+          )}
         </div>
       </div>
     </div>
