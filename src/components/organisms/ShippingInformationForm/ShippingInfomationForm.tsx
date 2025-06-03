cb7457147cba80bc25a9055e763234a77b439c20
KhuongVoTechbee <khuongvo@techbee.dev>
2025-06-03 18:05:56 +0700

    style: :lipstick: Fix uneven error of 2 select fields

--- src/components/organisms/ShippingInformationForm/ShippingInfomationForm.tsx
+++ src/components/organisms/ShippingInformationForm/ShippingInfomationForm.tsx
@@ -143,7 +143,7 @@ export const ShippingInfomationForm = ({ formObject }: ShippingInfomationFormPro
         />
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
         <Controller
           control={control}
           name="province"
