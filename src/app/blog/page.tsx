a421bfd61cc95c72ce700af4fe90a791cc04d37e
KhuongVoTechbee <khuongvo@techbee.dev>
2025-06-03 16:30:42 +0700

    style: :lipstick: Update the component style to match the design

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -43,7 +43,7 @@ export default async function BlogPage(props: { searchParams: SearchParams }) {
       <Breadcrumb items={[{ label: 'Blog', href: '/blog' }]} />
 
       {/* Danh sách bài viết */}
-      <div className="max-w-7xl mx-auto px-8 sm:px-16 lg:px-24 py-8">
+      <div className="flex flex-col justify-center items-center my-9">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
           {currentPosts.map((post: BlogPost) => (
             <BlogCard
--- src/components/organisms/BlogCard/BlogCard.tsx
+++ src/components/organisms/BlogCard/BlogCard.tsx
@@ -3,6 +3,8 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
+import { cn } from '@/lib/utils';
+
 export type BlogCardProps = {
   thumbnailUrl: string;
   title: string;
@@ -21,21 +23,26 @@ export const BlogCard = ({
   href
 }: BlogCardProps) => {
   return (
-    <div className="bg-white rounded shadow-sm flex flex-col">
-      <Link href={href} className="relative w-full h-48 rounded-t-[10px] overflow-hidden block">
-        <Image src={thumbnailUrl} alt={title} fill className="object-cover h-[260px] w-[380px]" />
+    <div className={cn('w-[379.8px] h-[492px]', 'bg-white rounded-[10px] shadow-sm flex flex-col')}>
+      <Link
+        href={href}
+        className="relative w-full h-[262px] rounded-t-[10px] overflow-hidden block mb-4"
+      >
+        <Image src={thumbnailUrl} alt={title} fill className="object-cover" />
       </Link>
-      <div className="p-4 flex flex-col flex-1">
-        <div className="text-xs text-gray-500 mb-1">
+      <div className="flex flex-col flex-1">
+        <div className="text-base text-typo-3 mb-1.5">
           {formatDate(new Date(postedDate), 'dd-MM-yyyy')} &nbsp;•&nbsp; {minRead} min read
         </div>
-        <Link href={href} className="hover:underline">
-          <h2 className="font-semibold text-base mb-2 line-clamp-2">{title}</h2>
+        <Link href={href} className="hover:underline mb-1.5">
+          <h2 className="font-semibold text-[22px] line-clamp-2">{title}</h2>
         </Link>
-        <p className="text-sm text-gray-600 mb-4 flex-1 line-clamp-2">{description}</p>
+        <div className="flex-1">
+          <p className="text-base text-typo-3 line-clamp-3">{description}</p>
+        </div>
         <Link
           href={href}
-          className="text-primary font-semibold text-sm flex items-center gap-1 hover:underline mt-auto"
+          className="text-primary font-semibold text-lg flex items-center gap-1 hover:underline"
         >
           Xem Thêm <span aria-hidden>→</span>
         </Link>
