8a90a615d8351a4e897aabfd03e7de46a811e354
BangVo123 <vochebang.123@gmail.com>
2025-06-03 11:56:11 +0700

    fix: line break in bp 1440px

--- src/components/templates/IntroductionContent/IntroductionContent.tsx
+++ src/components/templates/IntroductionContent/IntroductionContent.tsx
@@ -83,7 +83,7 @@ const IntroductionContent = ({ contactInfo }: IntroductionContentProps) => {
         >
           <div className="lg:grid lg:grid-cols-12">
             <div className="lg:col-span-10 lg:col-start-2">
-              <div className="flex flex-row flex-wrap justify-center gap-[24px] mt-[36px] xl:gap-[42px] 2xl:gap-[56px]">
+              <div className="flex flex-row flex-wrap justify-center gap-[16px] mt-[36px] 2xl:gap-[28px]">
                 {descriptions.guidingPrinciple.map((el, idx) => (
                   <GuidingCard
                     key={idx}
