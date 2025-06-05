f0b769025c6c5843f4ff93d6e1ea64870c913c9d
BangVo123 <vochebang.123@gmail.com>
2025-06-05 17:10:28 +0700

    fix: restore meta tag of home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,6 +37,7 @@ const RootLayout = ({ children }: RootLayoutProps) => {
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body
