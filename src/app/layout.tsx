45ee1579033f5d64d3db2cfc8d9337351743253a
BangVo123 <vochebang.123@gmail.com>
2025-06-05 16:01:18 +0700

    feat: register manifest file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,8 @@ import 'react-toastify/dist/ReactToastify.css';
 
 import { cn } from '@/lib/utils';
 
+import RegisterSW from './RegisterSW';
+
 const nunitoSans = Nunito_Sans({ subsets: ['latin'], variable: '--font-primary' });
 
 export async function generateMetadata(): Promise<Metadata> {
@@ -35,7 +37,7 @@ const RootLayout = ({ children }: RootLayoutProps) => {
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body
         className={cn(nunitoSans.variable, 'font-primary customscrollbar')}
@@ -43,6 +45,7 @@ const RootLayout = ({ children }: RootLayoutProps) => {
       >
         <MainProvider>
           <CartProvider>
+            <RegisterSW />
             <MainLayout>
               <main>{children}</main>
             </MainLayout>
