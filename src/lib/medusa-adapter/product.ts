bba2569471a9764da88db8d9312ac19022c29e65
BangVo123 <vochebang.123@gmail.com>
2025-06-09 16:45:51 +0700

    feat: add product transform func

--- src/lib/medusa-adapter/product.ts
+++ src/lib/medusa-adapter/product.ts
@@ -1,12 +1,67 @@
-import { Product } from '@/types/product';
+import { Product, ProductSpecifications, ProductVariant } from '@/types/product';
 import { HttpTypes } from '@medusajs/types';
 
 // TODO: Implement data transformation for products
 export const transformProduct = (product: HttpTypes.StoreProduct): Product => {
+  const ingredientRaw = product.metadata?.ingredient;
+  let ingredients;
+
+  if (typeof ingredientRaw === 'string') {
+    try {
+      ingredients = JSON.parse(ingredientRaw);
+    } catch {
+      ingredients = [];
+    }
+  } else {
+    ingredients = [];
+  }
+
+  const specs: ProductSpecifications[] = [
+    {
+      key: 'size',
+      value:
+        product.options?.[1].values?.map((el: HttpTypes.StoreProductOptionValue) => el.value) ?? []
+    },
+    {
+      key: 'savour',
+      value:
+        product.options?.[0].values?.map((el: HttpTypes.StoreProductOptionValue) => el.value) ?? []
+    }
+  ];
+
+  const variants: ProductVariant[] =
+    product.variants?.map((el: HttpTypes.StoreProductVariant): ProductVariant => {
+      const rank = product.metadata?.[el.sku];
+      const specsVar: Record<string, string> = {
+        size: String(el.options?.[0]?.value ?? ''),
+        savour: String(el.options?.[1]?.value ?? '')
+      };
+
+      return {
+        sku: el.sku,
+        name: el.title,
+        thumbnail:
+          product.images?.find((el: HttpTypes.StoreProductImage) => el.rank + 1 === rank)?.url ??
+          '',
+        specs: specsVar,
+        price: el.calculated_price?.calculated_amount,
+        isActive: !el.deleted_at
+      } as ProductVariant;
+    }) ?? [];
+
   const result = {
     id: product.id,
     name: product.title,
-    slug: product.handle
+    slug: product.handle,
+    price: variants?.[0].price,
+    categories: [product.collection_id],
+    thumbnail: product.thumbnail,
+    description: product.description,
+    ingredient: ingredients,
+    specs: specs,
+    variants: variants,
+    isNew: false,
+    createdAt: product.created_at
   };
 
   return result as Product;
