3c37998b1bff7382f19b43711db2434b8e6dcb0b
BangVo123 <vochebang.123@gmail.com>
2025-06-10 13:32:56 +0700

    feat: integrate highlight prods in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { QueryResourceResponse } from '@/types/common';
+import { Product } from '@/types/product';
 import { Quote as QuoteType } from '@/types/quote';
-import { getBestSellingProduct } from 'src/services/product.service';
+import { HttpTypes } from '@medusajs/types';
 import { getQuotes } from 'src/services/quote.service';
 
 import { Panel } from '@/components/atoms/Panel';
@@ -9,16 +10,34 @@ import { KnowledgeSection } from '@/components/organisms/KnowledgeSection';
 import { ProductSection } from '@/components/organisms/ProductSection';
 import { QuoteSection } from '@/components/organisms/QuoteSection';
 
+import { listProducts } from '@/lib/data/products';
+import { transformProduct } from '@/lib/medusa-adapter/product';
+
 const HomePage = async () => {
   try {
     const quotesResponse: QueryResourceResponse<QuoteType> = await getQuotes({ limit: 5 });
-    const bestSellingProducts = await getBestSellingProduct();
+    // const bestSellingProducts = await getBestSellingProduct();
+    const res = await listProducts({
+      pageParam: 1,
+      countryCode: process.env.NEXT_PUBLIC_DEFAULT_COUNTRY_CODE,
+      queryParams: {
+        limit: 8,
+        order: '-created_at',
+        offset: 0
+      }
+    });
+
+    const bestSellingProducts: Product[] = res.response.products.map(
+      (el: HttpTypes.StoreProduct) => {
+        return transformProduct(el);
+      }
+    );
 
     return (
       <div className="min-h-[1000px]">
         <Panel />
         <AboutSection />
-        <ProductSection initialBestSelling={bestSellingProducts.data} />
+        <ProductSection initialBestSelling={bestSellingProducts} />
         <QuoteSection initialQuotes={quotesResponse.data} />
         <KnowledgeSection />
       </div>
