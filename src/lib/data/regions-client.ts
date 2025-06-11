a54276fe866c1e5ee338d77d51a0ef9466e03a4e
quocma-tbs <quocma@techbee.dev>
2025-06-11 20:04:15 +0700

    feat: add client action for cart and regions

---
+++ src/lib/data/regions-client.ts
@@ -0,0 +1,43 @@
+import { HttpTypes } from '@medusajs/types';
+
+import { sdk } from '@/lib/medusa/medusa-config';
+import medusaError from '@/lib/medusa/util/medusa-error';
+
+export const listRegionsClient = async () => {
+  return sdk.client
+    .fetch<{ regions: HttpTypes.StoreRegion[] }>(`/store/regions`, {
+      method: 'GET'
+    })
+    .then(({ regions }) => regions)
+    .catch(medusaError);
+};
+
+const regionMap = new Map<string, HttpTypes.StoreRegion>();
+
+export const getRegionClient = async (countryCode: string) => {
+  try {
+    if (regionMap.has(countryCode)) {
+      return regionMap.get(countryCode);
+    }
+
+    const regions = await listRegionsClient();
+
+    if (!regions) {
+      return null;
+    }
+
+    regions.forEach((region) => {
+      region.countries?.forEach((c) => {
+        regionMap.set(c?.iso_2 ?? '', region);
+      });
+    });
+
+    const region = countryCode ? regionMap.get(countryCode) : regionMap.get('us');
+
+    return region;
+  } catch (e: unknown) {
+    console.error(e);
+
+    return null;
+  }
+};
