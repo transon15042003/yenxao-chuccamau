04ed67fd30fc5c1327154272d399168e3eb81368
quocma-tbs <quocma@techbee.dev>
2025-06-11 12:32:48 +0700

    feat: integrate api for product detail page

--- src/components/molecules/SliderControl/SliderControl.tsx
+++ src/components/molecules/SliderControl/SliderControl.tsx
@@ -2,7 +2,6 @@
 
 import { LargeChevronLeftSVG } from '@/svg/LargeChevronLeftSVG/LargeChevronLeftSVG';
 import { LargeChevronRightSVG } from '@/svg/LargeChevronRightSVG/LargeChevronRightSVG';
-import { useState } from 'react';
 
 import { Button } from '@/components/atoms/Button';
 import { useDetailProduct } from '@/components/providers/DetailProductProvider/DetailProductProvider';
@@ -13,49 +12,20 @@ interface SliderControlProps {
 }
 
 const SliderControl = ({ className }: SliderControlProps) => {
-  const [curIdx, setCurIdx] = useState<number>(0);
-  const { handleSetSize, handleSetFlavor, handleSetThumbnail, curSize, curFlavor, variants } =
-    useDetailProduct();
+  const { selectedVariant, variants, setSelectedVariant } = useDetailProduct();
+  const curIdx = variants.findIndex((el) => el.sku === selectedVariant?.sku);
 
-  const handleSetCurIdx = (type: 'increase' | 'decrease') => {
-    const length = variants.length;
-    const varIdx = variants.findIndex(
-      (el) => el.specs.size === curSize && el.specs.savour === curFlavor
-    );
+  const goBackVariant = () => {
+    if (curIdx > 0) {
+      const prevIdx = curIdx - 1;
+      setSelectedVariant(variants[prevIdx]);
+    }
+  };
 
-    switch (type) {
-      case 'increase':
-        setCurIdx((prev) => {
-          if (prev % length === length - 1) {
-            return 0;
-          } else return (prev % length) + 1;
-        });
-
-        if (varIdx !== -1 && varIdx !== undefined) {
-          const nextIdx = (varIdx + 1) % length;
-          handleSetSize(variants[nextIdx].specs.size);
-          handleSetFlavor(variants[nextIdx].specs.savour);
-          handleSetThumbnail(variants[nextIdx].thumbnail);
-        }
-        break;
-      case 'decrease':
-        setCurIdx((prev) => {
-          if (prev === 0) {
-            return length - 1;
-          }
-
-          return (prev % length) - 1;
-        });
-
-        if (varIdx !== -1 && varIdx !== undefined) {
-          const prevIdx = varIdx === 0 ? length - 1 : varIdx - 1;
-          handleSetSize(variants[prevIdx].specs.size);
-          handleSetFlavor(variants[prevIdx].specs.savour);
-          handleSetThumbnail(variants[prevIdx].thumbnail);
-        }
-        break;
-      default:
-        console.warn('Does not have ', type);
+  const goNextVariant = () => {
+    if (curIdx < variants.length - 1) {
+      const nextIdx = curIdx + 1;
+      setSelectedVariant(variants[nextIdx]);
     }
   };
 
@@ -64,7 +34,7 @@ const SliderControl = ({ className }: SliderControlProps) => {
       <Button
         variant="secondary"
         fill="outline"
-        onClick={() => handleSetCurIdx('decrease')}
+        onClick={goBackVariant}
         className="border-none w-auto"
       >
         <LargeChevronLeftSVG />
@@ -75,7 +45,7 @@ const SliderControl = ({ className }: SliderControlProps) => {
       <Button
         variant="secondary"
         fill="outline"
-        onClick={() => handleSetCurIdx('increase')}
+        onClick={goNextVariant}
         className="border-none w-auto"
       >
         <LargeChevronRightSVG />
