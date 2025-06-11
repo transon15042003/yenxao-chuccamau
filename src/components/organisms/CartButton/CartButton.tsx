7ba15963b1c989dc3ef9b1c5ab3edf8c7e1b4619
quocma-tbs <quocma@techbee.dev>
2025-06-11 12:34:14 +0700

    feat: hidden empty cart item badge

--- src/components/organisms/CartButton/CartButton.tsx
+++ src/components/organisms/CartButton/CartButton.tsx
@@ -9,9 +9,11 @@ interface CartButtonProps {
 const CartButton = ({ itemCount, onClick }: CartButtonProps) => (
   <div className="relative" onClick={onClick} role="button">
     <CartSVG className="w-6 h-6 md:w-7 md:h-7 text-white" />
-    <span className="absolute -top-2 -right-2 bg-secondary text-primary text-xs font-bold rounded-full px-1.5 py-0.5">
-      {itemCount}
-    </span>
+    {itemCount > 0 && (
+      <span className="absolute -top-2 -right-2 bg-secondary text-primary text-xs font-bold rounded-full px-1.5 py-0.5">
+        {itemCount}
+      </span>
+    )}
   </div>
 );
 
