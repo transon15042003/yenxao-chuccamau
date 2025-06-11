422dbfb2d917f93d406d0f512fae56a28260b956
quocma-tbs <quocma@techbee.dev>
2025-06-11 21:25:36 +0700

    fix: move fetch data logic out of main layout

--- src/components/templates/MainLayout/MainLayout.tsx
+++ src/components/templates/MainLayout/MainLayout.tsx
@@ -1,13 +1,9 @@
-import { Category } from '@/types/product';
-import { HttpTypes } from '@medusajs/types';
 import { ReactNode } from 'react';
 
 import { CartDrawer } from '@/components/organisms/CartDrawer/CartDrawer';
 import Footer from '@/components/organisms/Footer/Footer';
-import Header from '@/components/organisms/Header/Header';
+import HeaderWithCategories from '@/components/organisms/Header/HeaderWithCategories';
 
-import { listCategories } from '@/lib/data/categories';
-import { transformCategory } from '@/lib/medusa-adapter/category';
 import { cn } from '@/lib/utils';
 
 interface MainLayoutProps {
@@ -18,23 +14,11 @@ interface MainLayoutProps {
 // This is the place responsible for wrapping your app.
 // Add here components like Footer, Nav etc.
 export const MainLayout = async ({ children, className }: MainLayoutProps) => {
-  const categories = await listCategories();
-  const sortedCategories = categories.sort(
-    (a: HttpTypes.StoreProductCategory, b: HttpTypes.StoreProductCategory) => {
-      if (a.rank && b.rank) {
-        return a.rank - b.rank;
-      } else {
-        return 0;
-      }
-    }
-  );
-  const transformedCategories: Category[] = sortedCategories.map(transformCategory);
-
   const wrapperStyles = cn('flex flex-col min-h-screen', className);
 
   return (
     <div className={wrapperStyles}>
-      <Header categories={transformedCategories} />
+      <HeaderWithCategories />
       <main className="flex-1 mt-header-height">{children}</main>
       <Footer />
 
